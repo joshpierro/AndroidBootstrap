b287c786db29c602d192f96b3e9126d00f433524
josh <josh.pierro@gmail.com>
2015-07-08 23:44:05 -0700

    stubbed out master detail flow

---
+++ MasterDetailFlow/app/src/main/java/pierro/dallett/josh/masterdetailflow/ArtistListActivity.java
@@ -0,0 +1,114 @@
+package pierro.dallett.josh.masterdetailflow;
+
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+
+/**
+ * An activity representing a list of Artists. This activity
+ * has different presentations for handset and tablet-size devices. On
+ * handsets, the activity presents a list of items, which when touched,
+ * lead to a {@link ArtistDetailActivity} representing
+ * item details. On tablets, the activity presents the list of items and
+ * item details side-by-side using two vertical panes.
+ * <p/>
+ * The activity makes heavy use of fragments. The list of items is a
+ * {@link ArtistListFragment} and the item details
+ * (if present) is a {@link ArtistDetailFragment}.
+ * <p/>
+ * This activity also implements the required
+ * {@link ArtistListFragment.Callbacks} interface
+ * to listen for item selections.
+ */
+public class ArtistListActivity extends AppCompatActivity
+        implements ArtistListFragment.Callbacks {
+
+    /**
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+     * device.
+     */
+    private boolean mTwoPane;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
+    }
+
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        // handleIntent(intent);
+        Log.i("xx", "got intent");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_artist_list);
+
+        getSupportActionBar();
+
+        if (findViewById(R.id.artist_detail_container) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-large and
+            // res/values-sw600dp). If this view is present, then the
+            // activity should be in two-pane mode.
+            mTwoPane = true;
+
+            // In two-pane mode, list items should be given the
+            // 'activated' state when touched.
+            ((ArtistListFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.artist_list))
+                    .setActivateOnItemClick(true);
+        }
+
+        // TODO: If exposing deep links into your app, handle intents here.
+    }
+
+    /**
+     * Callback method from {@link ArtistListFragment.Callbacks}
+     * indicating that the item with the given ID was selected.
+     */
+
+
+
+    @Override
+    public void onItemSelected(String id) {
+        if (mTwoPane) {
+            // In two-pane mode, show the detail view in this activity by
+            // adding or replacing the detail fragment using a
+            // fragment transaction.
+            Bundle arguments = new Bundle();
+            arguments.putString(ArtistDetailFragment.ARG_ITEM_ID, id);
+            ArtistDetailFragment fragment = new ArtistDetailFragment();
+            fragment.setArguments(arguments);
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.artist_detail_container, fragment)
+                    .commit();
+
+        } else {
+            // In single-pane mode, simply start the detail activity
+            // for the selected item ID.
+            Intent detailIntent = new Intent(this, ArtistDetailActivity.class);
+            detailIntent.putExtra(ArtistDetailFragment.ARG_ITEM_ID, id);
+            startActivity(detailIntent);
+        }
+    }
+}
