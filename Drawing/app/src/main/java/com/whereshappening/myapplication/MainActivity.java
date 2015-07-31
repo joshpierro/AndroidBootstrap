bcbeaec6d758ee6586443f0a47ddfb14539d9036
josh <josh.pierro@gmail.com>
2015-07-30 22:03:08 -0700

    stubbed out drawing app

---
+++ Drawing/app/src/main/java/com/whereshappening/myapplication/MainActivity.java
@@ -0,0 +1,77 @@
+package com.whereshappening.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import com.whereshappening.myapplication.drawing.DrawingView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private DrawingView drawView;
+    private ImageButton currPaint;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //get drawing view
+        drawView = (DrawingView)findViewById(R.id.drawing);
+
+        //get the palette and first color button
+        LinearLayout paintLayout = (LinearLayout)findViewById(R.id.paint_colors);
+        currPaint = (ImageButton)paintLayout.getChildAt(0);
+        currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    //user clicked paint
+    public void paintClicked(View view){
+        //use chosen color
+
+        //set erase false
+        drawView.setErase(false);
+        drawView.setBrushSize(drawView.getLastBrushSize());
+
+        if(view!=currPaint){
+            ImageButton imgView = (ImageButton)view;
+            String color = view.getTag().toString();
+            drawView.setColor(color);
+            //update ui
+            imgView.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+            currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint));
+            currPaint=(ImageButton)view;
+        }
+    }
+
+
+}
--- MasterDetailFlow/app/src/main/java/pierro/dallett/josh/masterdetailflow/data/Artist.java
+++
@@ -1,9 +0,0 @@
-package pierro.dallett.josh.masterdetailflow.data;
-
-/**
- * Created by jpierro on 7/10/2015.
- */
-public class Artist {
-    public int Id;
-    public String Artist;
-}
